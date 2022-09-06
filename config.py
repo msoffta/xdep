ca2a8ebe542a12d652c8479cb5fe1c36749ac13a
MsOffta <lnoutui@gmail.com>
2022-09-06 16:44:05 +0500

    Release

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "5743203235:AAHsBSUu2b2wdKp-tP2RK1T33dsrzYxOpr4"
-BOT_OWNER = 1850498089
-BOT_LOG = -1001674557576
+# group ids or account ids can be retrieved with @username_to_id_bot
+BOT_TOKEN="<bot-token>"
+BOT_OWNER=<bot-owner-id>
