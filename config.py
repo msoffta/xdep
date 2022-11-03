c3004df8298a268679cb494d2af6db9852dec40a
msoffta <lnoutui@gmail.com>
2022-11-03 12:42:56 +0500

    SuperRelease

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # group ids or account ids can be retrieved with @username_to_id_bot
-BOT_TOKEN="5743203235:AAHsBSUu2b2wdKp-tP2RK1T33dsrzYxOpr4"
-BOT_OWNER= 1850498089
-BOT_LOG = -1001674557576
+BOT_TOKEN = "<id>"
+BOT_OWNER = <id>
+BOT_LOG = <id>
 
